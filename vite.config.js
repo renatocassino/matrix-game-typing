9a3687a5f5f93ed00b81c60f2aebff776245fceb
Cassino <renatocassino@gmail.com>
2023-09-27 00:32:26 -0300

    fix: fix build page

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ const ASSET_URL = process.env.ASSET_URL || '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  env: {
-    base: ASSET_URL,
-  },
+  base: ASSET_URL,
   plugins: [vue()],
 });
