adc0b3e5cf321f815db434d9a13b4fff7624e523
Renato Cassino <renatocassino@gmail.com>
2024-05-20 12:08:14 -0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,17 @@
 
 <section>
   <Hero />
+
+  <script
+    data-name="BMC-Widget"
+    data-cfasync="false"
+    src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+    data-id="cassinodev"
+    data-description="Support me on Buy me a coffee!"
+    data-message="Thank you for playing! 😊 If you'd like to support me, buying me a coffee would make my day! ☕️"
+    data-color="#40DCA5"
+    data-position="Right"
+    data-x_margin="18"
+    data-y_margin="18"
+  ></script>
 </section>
