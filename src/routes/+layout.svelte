4791f3c4ff924774911bd74540a6df6efa429008
Renato Cassino <renatocassino@gmail.com>
2024-05-20 11:42:26 -0300

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -88,9 +88,5 @@
     src="https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser-arcade-physics.min.js"
   ></script>-->
 
-  <div style="text-align: center; width: 100%; padding: 1rem;">
-    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="cassinodev" data-color="#40DCA5" data-emoji="☕"  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
-  </div>
-
   <Footer />
 </div>
