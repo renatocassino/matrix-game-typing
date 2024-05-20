a6fa8a3889967b52ddcfee166f969266bd00bddc
Renato Cassino <renatocassino@gmail.com>
2024-05-20 12:07:20 -0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,4 @@
 
 <section>
   <Hero />
-
-  <div style="text-align: center; width: 100%; padding: 1rem;">
-    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="cassinodev" data-color="#40DCA5" data-emoji="☕"  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
-  </div>
 </section>
